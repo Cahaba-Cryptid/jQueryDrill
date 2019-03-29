5f24dcadc0839b04bf5c9b33bd4bb7bab2fd8fb9
Hayden <chfrank9011@gmail.com>
2019-03-29 15:45:26 -0500

    final commit

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
-$('#submitBtn').click(function(){
-    $('#targetDiv').append('<h2 id=targetH2>' + $('submitBox'.val()) + '</h2>');
+$('#submitBtn').click(function () {
+    event.preventDefault();
+    let newLi = $('<li class="targetLi">' + $("#submitBox").val() + '</li>');
+    newLi.appendTo('#targetUl');
+    newLi.click(function () {
+        newLi.css('color', randoRainbow());
+        newLi.dblclick(function(){
+            newLi.remove();
+        });
+    });
+    //$("<h2 id='targerH2'>" + $("#submitBox").val() + "</h2>").appendTo('#targetDiv');
 });
 
 $('#submitBtn').attr('disabled', true);
 
 $('#submitBox').keyup(function () {
-    if ($('#submitBox').val().length != 0)
+    if ($('#submitBox').val().length != 0) {
         $('#submitBtn').attr('disabled', false);
-    else
+    } else {
         $('#submitBtn').attr('disabled', true);
+    }
 });
 
 $('body').append('<div id="targetDiv"></div>');
+
+$('body').append('<ul id="targetUl"></ul>');
+
+function randoRainbow() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+} 
 
